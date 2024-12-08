8e42637beb3110344a96b8fea57c9fea56647ce6
Tomo Krajina <tkrajina@gmail.com>
2024-12-08 17:53:42 +0100

    Quarter of board (for assembling parts)

--- goban.py
+++ goban.py
@@ -8,6 +8,8 @@ diameter = int(os.environ.get("DIAMETER", 18))
 height = int(os.environ.get("HEIGHT", 6))
 n = int(os.environ.get("N", 9))
 
+around = 2
+
 stones_grid_rows = int(os.environ.get("ROWS", 10))
 stones_grid_cols = int(os.environ.get("COLS", 9))
 
@@ -66,24 +68,6 @@ def goban_hoshi():
 		all_hoshis.append(hoshi(coords[0], coords[1]))
 	return s2.union()(all_hoshis)
 
-"""
-module board_stones() {
-    for (i = [0 : 1 : n-1])
-    {
-        for (j = [0 : 1 : n-1])
-        {
-            translate([diameter * i, diameter * j, 1])
-            {
-                minkowski() {
-                    stone();
-                    sphere(1);
-                }
-            }
-        }
-    }
-}
-"""
-
 def board_stones():
 	stones = []
 	for row in range(n):
@@ -106,9 +90,11 @@ def stones_grid(rows, cols):
 			stones.append(s2.translate([row*d+(h if col%2==0 else -h), col*v, 0])(stone()))
 	return s2.union()(stones)
 
-def board():
-	board_height = height / 3
-	radius = diameter / 2 + 2
+def board(board_height=1, hole_depth=None):
+	if not hole_depth:
+		hole_depth = height / 4
+	board_height = board_height + hole_depth
+	radius = diameter / 2 + around
 	return s2.difference()(
 		s2.hull()(
 			s2.cylinder(board_height, radius, radius),
@@ -117,22 +103,38 @@ def board():
 			s2.cylinder(board_height, radius, radius).translate([0, (n-1)*diameter, 0]),
 		),
 		s2.union()(
-			s2.translate([0, 0, 1])(board_stones()),
-			s2.translate([0, 0, 0.6])(grid()),
-			s2.translate([0, 0, 0.6])(goban_hoshi()),
+			s2.translate([0, 0, board_height-hole_depth])(board_stones()),
+			s2.translate([0, 0, board_height-hole_depth-.3])(grid()),
+			s2.translate([0, 0, board_height-hole_depth-.3])(goban_hoshi()),
 		)
 	)
 
 def grid():
-	w = 0.5
+	w = 0.75
 	grid = []
 	for i in range(n):
-		grid.append(s2.translate([i*diameter - w/2, -w/2, 0])(s2.cube([w, (n-1)*diameter + w, w])))
-		grid.append(s2.translate([-w/2, i*diameter-w/2, 0])(s2.cube([(n-1)*diameter, w, w]))
+		grid.append(s2.translate([i*diameter - w/2, -w/2, 0])(s2.cube([w, (n-1)*diameter + w, 10])))
+		grid.append(s2.translate([-w/2, i*diameter-w/2, 0])(s2.cube([(n-1)*diameter, w, 10]))
 	)
 	return s2.union()(grid)
 
+def quarter_board():
+	side = (n*diameter+2*around)/2
+	res = board(6).translate([diameter/2+around, diameter/2+around, 0]) * s2.cube([side, side, 100])
+	return res \
+		+ hook().rotate([0, 0, 90]).translate([side, side/2, 0]) \
+		- s2.minkowski()(hook(), s2.sphere(.4)).translate([side/2, side, 0]) \
 
+def hook():
+	hook = s2.cube([10, 10, 2], center=True) \
+		- s2.cylinder(2.5, 4, 4, center=True).translate([6, 0, 0]) \
+		- s2.cylinder(2.5, 4, 4, center=True).translate([-6, 0, 0])
+	return hook.translate([0, 0, 1])
+
+s2.scad_render_to_file(hook(), "_hook.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
 s2.scad_render_to_file(board(), "board.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
+s2.scad_render_to_file(quarter_board(), "board_assemble.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
+s2.scad_render_to_file(board(board_height=8), "board_thick.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
+s2.scad_render_to_file(quarter_board(), "board_assemble.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
 s2.scad_render_to_file(stone(), "stone.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
 s2.scad_render_to_file(stones_grid(stones_grid_cols, stones_grid_rows), "stones.scad", out_dir="scad", file_header="$fn = $preview ? 20 : 60;")
